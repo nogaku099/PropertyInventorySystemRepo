bf124e510ffa666d561d45a9dd2bacf2fa515651
Ted <tuantrong2007@gmail.com>
2024-11-12 16:40:31 +0700

    AllowAllCors

--- PropertyInventorySystem.API/Program.cs
+++ PropertyInventorySystem.API/Program.cs
@@ -27,6 +27,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -36,6 +44,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowAll");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
